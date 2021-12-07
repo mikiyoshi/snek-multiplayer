8086ae48433c461811dbc820ef026419d8ca490b
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-07 18:45:08 +0000

    port module update

--- client.js
+++ client.js
@@ -1,11 +1,15 @@
 const net = require('net');
-// const { IP, PORT } = require('./constants');
 
 // establishes a connection with the game server
 const connect = function () {
+  const { host, port } = require('./constants');
+  // console.log(IP);
+  // console.log(PORT);
+  // const ipNum = IP();
+  // const postData = PORT();
   const conn = net.createConnection({
-    host: '10.0.2.15', // IP address here,
-    port: 50541, // PORT number here,
+    host: host, // IP address here,
+    port: port, // PORT number here,
   });
 
   // interpret incoming data as text
---
+++ constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+  host: '10.0.2.15', // IP address here,
+  port: 50541, // PORT number here,
+};
